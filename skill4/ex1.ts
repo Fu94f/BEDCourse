db2ff98da9881627dc227e998df36d79c9ec5019
Fu94f <fulvia.franco1994@gmail.com>
2026-08-28 16:30:03 +0200

    add ex1 skill4

---
+++ skill4/ex1.ts
@@ -0,0 +1,42 @@
+//FILTER ONLY
+
+interface Product{
+    id:number;
+    name:string;
+    price:number;
+    tags:string[];
+}
+
+
+const products: Product[] =[
+{ 
+    id: 1, 
+    name: "Laptop", 
+    price: 12000, 
+    tags: ["work", "power"] 
+},
+{ 
+    id: 2,
+    name: "Mouse",
+    price: 750,
+    tags: ["accessory"] 
+},
+{   id: 3,
+    name: "Keyboard", 
+    price: 1000, 
+    tags: ["accessory", "work"] 
+},
+{   id: 4, 
+    name: "Monitor", 
+    price: 4500, 
+    tags: ["work", "display"] 
+},
+{   id: 5, 
+    name: "Headphones", 
+    price: 1500, 
+    tags: ["audio", "accessory"] 
+}
+];
+
+console.log(products.filter((p)=> p.price < 1000));
+
