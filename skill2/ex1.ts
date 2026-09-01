09b1ae448bad0b7f6b9126ab8be423ea25e2b557
Fu94f <fulvia.franco1994@gmail.com>
2026-09-01 15:03:22 +0200

    add ex1 skill2

---
+++ skill2/ex1.ts
@@ -0,0 +1,18 @@
+// INTERFACE AND TYPES ALIASES
+
+// BOOK INTERFACE
+
+interface Book {
+    title:string;
+    pages:number;
+
+}
+
+const describeBook = (book : Book) =>{
+    console.log(`The book ${book.title} has ${book.pages} pages.`)
+}
+
+describeBook({
+    title:"Dune",
+    pages:412,
+});
