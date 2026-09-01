8df541d814926c6b708291137e64f7261805f498
Fu94f <fulvia.franco1994@gmail.com>
2026-09-01 15:26:56 +0200

    add ex2 skill2

---
+++ skill2/ex2.ts
@@ -0,0 +1,25 @@
+// COMBINING INTERFACES
+
+interface Teacher {
+    name: string;
+    subject: string;
+}
+
+interface Employee {
+    id:number;
+    email:string;
+}
+
+type SchoolTeacher = Teacher & Employee;
+
+
+const printTeacherInfo = (schoolTeacher: SchoolTeacher) =>{
+    console.log(`Name: ${schoolTeacher.name}, Subject: ${schoolTeacher.subject}, ID: ${schoolTeacher.id}, Email: ${schoolTeacher.email} `)
+}
+
+printTeacherInfo({
+    name :"Marina",
+    subject: "English",
+    id: 12345,
+    email: "marina@school.se"
+})
