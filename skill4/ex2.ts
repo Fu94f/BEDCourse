a90c65ee463805f6de61ad66e1ef6a92a4ae3ab1
Fu94f <fulvia.franco1994@gmail.com>
2026-09-04 10:56:18 +0200

    remove copies of files

--- skill4/ex2.ts
+++
@@ -1,8 +0,0 @@
-// FIRST IN LINE
-
-const firstItem = <T>(array: T[]): T => {
-    return array[0]!;
-}
-
-console.log(firstItem(["a", "b", "c"]));
-console.log(firstItem([1, 2, 3]));
--- skill4/ex3.ts
+++
@@ -1,8 +0,0 @@
-//SWAP PLACES
-
-const swap = <T> (item1:T, item2:T): T[] =>{
-    return [item1, item2].reverse();
-}
-
-console.log (swap("hello", "hi"));
-console.log (swap(1, 2));
