0fbf80f207bb9438a151bab3a72f9278d4bab02c
Fu94f <fulvia.franco1994@gmail.com>
2026-09-08 20:01:53 +0200

    touch up

--- skill2/ex1.ts
+++ skill2/ex1.ts
@@ -8,6 +8,23 @@
 // Run it and, in a comment above your code, write down the actual order the three logs appear in,
 //  and explain in one sentence why that order happens.
 
+
+// COMMENT
+
+//Order:
+//1 Before countdown 
+//2 after countdown
+//3 Time's up!
+//4 Countdown finished.
+
+// console.log("before countdown")prints first beca
+// use it's the first "in line",
+// then countdown()gets called but because there is setTimout it doesnt stop to wait,
+// moves on to the next one which is console.log("after countdown").
+// setTimout "schedules" the action for amount of time assigned and prints after 3 sec.
+// This is also the reason why "Time's up!"and "Countdown finished" are the last ones to be printed.
+
+
 const countdown = (seconds : number, callback: () => void) => {
     setTimeout(()=> {
         console.log("Time's up!");
